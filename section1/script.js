e8dcd73cafc407bd511217c8c3ce60832372a70c
ManojSilag <manojsilag@gmail.com>
2020-05-28 20:07:10 +0530

    typeScript compiling

---
+++ section1/script.js
@@ -0,0 +1,5 @@
+var character = "mariooooo";
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log('dev: inputs', inputs);
+inputs.forEach(function (input) { return console.log(input); });
---
+++ section1/script.ts
@@ -0,0 +1,9 @@
+const character = "mariooooo";
+
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input')
+console.log('dev: inputs', inputs)
+
+inputs.forEach(input => console.log(input))
