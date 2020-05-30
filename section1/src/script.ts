36f5a1888ff7f621f1e164fe7d332259cafee1e5
ManojSilag <manojsilag@gmail.com>
2020-05-30 20:56:13 +0530

    function signature

--- section1/src/script.ts
+++ section1/src/script.ts
@@ -1,29 +1,9 @@
-let greet = () => {
-  console.log("Heloo world");
+// let greet ; Function;
+
+let greet: (a: string, b: string) => void;
+
+greet = (name: string, age: string) => {
+  console.log(name, age);
+  return 43;
 };
 
-let greeting: Function;
-
-// greeting = 'dvs';
-
-greeting = () => {
-  console.log("this is function");
-};
-
-const add = (a: number, b: number, c: number | string = 10) => {
-  console.log(a + b);
-  console.log(c);
-};
-
-add(4, 3);
-add(9, 3, "fdfgdds");
-
-const minus = (a: number, b: number): number => {
-  return a - b;
-};
-
-let result = minus(10, 17);
-
-console.log("dev: result", result);
-
-// result = "svcadv"
