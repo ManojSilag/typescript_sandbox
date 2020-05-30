364945007b73375a1860ccaed913af10eeb6fa05
ManojSilag <manojsilag@gmail.com>
2020-05-30 20:39:31 +0530

    function basics

--- section1/public/script.js
+++ section1/public/script.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log('test');
+var greet = function () {
+    console.log("Heloo world");
+};
+var greeting;
+// greeting = 'dvs';
+greeting = function () {
+    console.log("this is function");
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(4, 3);
+add(9, 3, "fdfgdds");
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(10, 17);
+console.log("dev: result", result);
+// result = "svcadv"
--- section1/src/script.ts
+++ section1/src/script.ts
@@ -1,1 +1,29 @@
-console.log('test');
+let greet = () => {
+  console.log("Heloo world");
+};
+
+let greeting: Function;
+
+// greeting = 'dvs';
+
+greeting = () => {
+  console.log("this is function");
+};
+
+const add = (a: number, b: number, c: number | string = 10) => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(4, 3);
+add(9, 3, "fdfgdds");
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+};
+
+let result = minus(10, 17);
+
+console.log("dev: result", result);
+
+// result = "svcadv"
