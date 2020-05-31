a32b72e68bb5310f4fd4460479f491cd860abf29
ManojSilag <manojsilag@gmail.com>
2020-05-31 17:51:00 +0530

    The DOM and Type Casting

--- section1/public/script.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// let greet ; Function;
-var greet;
-greet = function (name, age) {
-    console.log(name, age);
-    return 43;
-};
--- section1/src/script.ts
+++
@@ -1,9 +0,0 @@
-// let greet ; Function;
-
-let greet: (a: string, b: string) => void;
-
-greet = (name: string, age: string) => {
-  console.log(name, age);
-  return 43;
-};
-
