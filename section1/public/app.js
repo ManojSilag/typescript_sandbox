d9388d6c6cc1c1377cbe6a6c24bc831673920730
ManojSilag <manojsilag@gmail.com>
2020-06-05 20:03:36 +0530

    Interface with classes and Render HTML

--- section1/public/app.js
+++ section1/public/app.js
@@ -1,33 +1,62 @@
 import { Invoice } from "./classes/Invoice.js";
-const InvoiceOne = new Invoice("mario", "works on mario", 200);
-const InvoiceTwo = new Invoice("anuj", "works on anuj", 400);
-let invoices = [];
-invoices.push(InvoiceOne);
-invoices.push(InvoiceTwo);
-const me = {
-    name: "manoj",
-    age: 12,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log(`I spent `, amount);
-        return amount;
-    }
-};
-console.log(me);
-me.speak("yoyoyo");
-me.spend(1232);
-const greetPerson = (person) => {
-    console.log('hello', person.name);
-};
-greetPerson(me);
+import { Payment } from "./classes/Payment.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice("Man", "work", 240);
+// docTwo = new Payment("nonono", "lots work", 140);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
+// const InvoiceOne = new Invoice("mario", "works on mario", 200);
+// const InvoiceTwo = new Invoice("anuj", "works on anuj", 400);
+// let invoices: Invoice[] = [];
+// invoices.push(InvoiceOne);
+// invoices.push(InvoiceTwo);
+// // console.log(invoices);
+// interface IsPerson {
+//   name: string;
+//   age: number;
+//   speak(a: string): void;
+//   spend(a: number): number;
+// }
+// const me: IsPerson = {
+//   name: "manoj",
+//   age: 12,
+//   speak(text: string): void {
+//     console.log(text);
+//   },
+//   spend(amount: number): number {
+//     console.log(`I spent `, amount);
+//     return amount;
+//   }
+// };
+// console.log(me);
+// me.speak("yoyoyo");
+// me.spend(1232);
+// const greetPerson = (person: IsPerson) => {
+//   console.log("hello", person.name);
+// };
+// greetPerson(me);
 const form = document.querySelector(".new-item-form");
-const types = document.querySelector("#type");
+const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(types.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
+    list.render(doc, type.value, "end");
+    // console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- section1/src/app.ts
+++ section1/src/app.ts
@@ -1,49 +1,77 @@
 import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./Interfaces/HasFormatter.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
 
-const InvoiceOne = new Invoice("mario", "works on mario", 200);
-const InvoiceTwo = new Invoice("anuj", "works on anuj", 400);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
 
-let invoices: Invoice[] = [];
-invoices.push(InvoiceOne);
-invoices.push(InvoiceTwo);
-// console.log(invoices);
+// docOne = new Invoice("Man", "work", 240);
+// docTwo = new Payment("nonono", "lots work", 140);
 
-interface IsPerson {
-  name: string;
-  age: number;
-  speak(a: string): void;
-  spend(a: number): number;
-}
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
 
-const me: IsPerson = {
-  name: "manoj",
-  age: 12,
-  speak(text: string): void {
-    console.log(text);
-  },
-  spend(amount: number): number {
-    console.log(`I spent `, amount);
-    return amount;
-  }
-};
-console.log(me);
-me.speak("yoyoyo");
-me.spend(1232);
+// console.log(docs);
 
-const greetPerson = (person: IsPerson) =>{
-  console.log('hello', person.name);
-}
+// const InvoiceOne = new Invoice("mario", "works on mario", 200);
+// const InvoiceTwo = new Invoice("anuj", "works on anuj", 400);
 
-greetPerson(me)
+// let invoices: Invoice[] = [];
+// invoices.push(InvoiceOne);
+// invoices.push(InvoiceTwo);
+// // console.log(invoices);
+
+// interface IsPerson {
+//   name: string;
+//   age: number;
+//   speak(a: string): void;
+//   spend(a: number): number;
+// }
+
+// const me: IsPerson = {
+//   name: "manoj",
+//   age: 12,
+//   speak(text: string): void {
+//     console.log(text);
+//   },
+//   spend(amount: number): number {
+//     console.log(`I spent `, amount);
+//     return amount;
+//   }
+// };
+// console.log(me);
+// me.speak("yoyoyo");
+// me.spend(1232);
+
+// const greetPerson = (person: IsPerson) => {
+//   console.log("hello", person.name);
+// };
+
+// greetPerson(me);
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
-const types = document.querySelector("#type") as HTMLSelectElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log(types.value, tofrom.value, details.value, amount.valueAsNumber);
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+  console.log(doc);
+  list.render(doc, type.value, "end");
+
+  // console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
