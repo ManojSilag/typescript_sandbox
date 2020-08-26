c3386f3acbff7c66a6756c8ef4f682bfca644405
ManojSilag <manojsilag@gmail.com>
2020-08-26 13:36:59 +0530

    custom map

--- section4/src/index.ts
+++ section4/src/index.ts
@@ -1,17 +1,4 @@
 import { User } from "./User";
 import { Company } from "./Company";
-import {} from "@types/googlemaps";
-
-const user = new User();
-const comp = new Company();
-
-// console.log(comp);
-// console.log(user);
-
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+import { CustomMap } from "./CustomMap";
+new CustomMap("map")
