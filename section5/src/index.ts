e22cc9f3e5938d6d6ae77925b073231a2febd91a
ManojSilag <manojsilag@gmail.com>
2020-09-02 19:40:10 +0530

    type guards

--- section5/src/index.ts
+++ section5/src/index.ts
@@ -1,8 +1,27 @@
-console.log("Hi there 1234");
+class Sorter {
+  // collection: number[];
+  // constructor( collection: number[]){
+  //   this.collection = collection;
+  // }
 
-const logSomething = () => {
-  console.log("something");
-};
+  constructor(public collection: number[]) {}
 
-logSomething();
+  sort(): void {
+    const length = this.collection.length;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
 
