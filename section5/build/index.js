cfdc70e161c3b5b621f2a2d59ae372fdfa6ff7f3
ManojSilag <manojsilag@gmail.com>
2020-09-06 19:24:46 +0530

    abstract classes

--- section5/build/index.js
+++ section5/build/index.js
@@ -1,20 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
 var LinkedList_1 = require("./LinkedList");
-// const numberCollection = new NumbersCollection([10,3,-5,0])
-// const sorter = new Sorter(numberCollection);
-// sorter.sort();
+// const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+// numberCollection.sort();
 // console.log(numberCollection.data);
 // const characterCollection = new CharacterCollection("manoj")
-// const sorter = new Sorter(characterCollection);
-// sorter.sort();
+// characterCollection.sort();
 // console.log(characterCollection.data);
 var linkedList = new LinkedList_1.LinkedList();
 linkedList.add(500);
 linkedList.add(570);
 linkedList.add(-20);
 linkedList.add(0);
-var sorter = new Sorter_1.Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
--- section5/src/index.ts
+++ section5/src/index.ts
@@ -1,26 +1,20 @@
-import { Sorter }from "./Sorter";
-import { NumbersCollection } from "./NumberCollection"
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumberCollection";
 import { CharacterCollection } from "./CharacterCollection";
 import { LinkedList } from "./LinkedList";
 
-
-
-// const numberCollection = new NumbersCollection([10,3,-5,0])
-// const sorter = new Sorter(numberCollection);
-// sorter.sort();
+// const numberCollection = new NumbersCollection([10, 3, -5, 0]);
+// numberCollection.sort();
 // console.log(numberCollection.data);
 
 // const characterCollection = new CharacterCollection("manoj")
-// const sorter = new Sorter(characterCollection);
-// sorter.sort();
+// characterCollection.sort();
 // console.log(characterCollection.data);
-
 
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(570);
 linkedList.add(-20);
 linkedList.add(0);
-const sorter = new Sorter(linkedList);
-sorter.sort();
-linkedList.print()
+linkedList.sort();
+linkedList.print();
