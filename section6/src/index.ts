f675fffa953930dc58f21ba5ef2c601a1214fd4f
ManojSilag <manojsilag@gmail.com>
2020-09-08 17:39:31 +0530

    using enum and creating class to read file

--- section6/src/index.ts
+++ section6/src/index.ts
@@ -1,12 +1,21 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-console.log("hi there");
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8"
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
-console.log(matches);
+const reader = new CsvFileReader("football.csv");
+reader.read();
+
+let manUnitedWins = 0;
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D"
+}
+
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
