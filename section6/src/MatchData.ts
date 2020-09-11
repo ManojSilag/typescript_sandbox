4d7b78e2359c91692ff48665ed8d288a7660c646
ManojSilag <manojsilag@gmail.com>
2020-09-11 17:29:16 +0530

    compostion based aproch

---
+++ section6/src/MatchData.ts
@@ -0,0 +1,12 @@
+import { MatchReader } from "./MatchReader";
+import { MatchResult } from "./MatchResult";
+
+export type MatchData = [
+  Date,
+  string,
+  string,
+  number,
+  number,
+  MatchResult,
+  string
+];
